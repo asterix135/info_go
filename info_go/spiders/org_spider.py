2af606f4943311f5abace8a5f0c3550f0f14e7e8
asterix135 <chgraham@gmail.com>
2015-06-25 17:59:58 -0400

    pers_spider working except for file write

--- info_go/spiders/org_spider.py
+++ info_go/spiders/org_spider.py
@@ -6,11 +6,11 @@ class InfoGoOrgSpider(scrapy.Spider):
     '''Spider that goes through all organization pages and pulls id numbers for employee info scraping'''
     name = "org"
     allowed_domains = ["infogo.gov.on.ca"]
-    min_num = 589 # should be 2
-    max_num = 591 # should be 7356
+    min_num = 2 # should be 2
+    max_num = 3 # should be 7356
 
     def start_requests(self):
-        for page in range(self.min_num, self.max_num):
+        for page in range(self.min_num, self.max_num + 1):
             yield Request('http://www.infogo.gov.on.ca/infogo/office.do?actionType=telephonedirectory&unitId=%d' % page,
                     callback = self.parse)
 
@@ -18,13 +18,13 @@ class InfoGoOrgSpider(scrapy.Spider):
     def parse(self, response):
         file_dest = open('employee_numbers.txt', 'a')
 
-        for sel in response.xpath('//td[@class="content"]'):
+        #for sel in response.xpath('//td[@class="content"]'):
+        for sel in response.xpath('//body'):
             employee_id = response.xpath('//a[@class="employee"]/@href').extract()
             print "Employee id"
             print employee_id
             for item in employee_id:
                 id_number = re.match("(.*)Employee\(\'(\d+)(.*)", str(item)).group(2)
-                print id_number
                 file_dest.write(id_number + '\n')
         file_dest.close()
 
--- info_go/spiders/pers_spider.py
+++ info_go/spiders/pers_spider.py
@@ -6,39 +6,60 @@ import os.path
 class InfoGoPersSpider(scrapy.Spider):
     name = "person"
     allowed_domains = ["infogo.gov.on.ca"]
-    if os.path.exists('employee_numbers.txt'):
-        employee_set = set(line.strip() for line in open('employee_numbers.txt'))
-    else:
-        employee_set = set(['23895'])
+
+    #activate when testing done
+    #if os.path.exists('employee_numbers.txt'):
+    #    employee_set = set(line.strip() for line in open('employee_numbers.txt'))
+    #else:
+    #    employee_set = set(['23895'])
+
+    employee_set = set(['23444'])
 
     def start_requests(self):
         for page in self.employee_set:
-            yield Request('http://www.infogo.gov.on.ca/infogo/employee.do?actionType=browse&Id=%s' % page,
+            yield Request('http://www.infogo.gov.on.ca/infogo/employee.do?actionType=browse&id=%s' % page,
                     callback = self.parse)
 
     def parse(self, response):
         file_dest = open('employees.txt', 'a')
-        deleteme = response.xpath('//td[@class="content"]/font').extract()
-        print "Deleteme"
-        print deleteme
-        file_dest.write('\n'.join(deleteme))
+        #file_dest.write('\n'.join(deleteme))
 
-        for sel in response.xpath('//td[@class="content"]'):
-            employee_details = response.xpath('//font').extract()
-            hierarchy_details = response.xpath('//a[@href]').extract()
-            #both of these are lists - need to extract info and regex it
+        for sel in response.xpath('//table[@title="employeeinfo_frame"]'):
+            employee_details = response.xpath('//td[@class="content"]/font').extract()
+            email_extract = response.xpath('//td[@class="content"]/a[@href]').extract()
+            org_name_extract = response.xpath('//td[@class="content"]/ul/a[@href]').extract()
+            hierarchy_extract = response.xpath('//td[@class="content"]//ul/a[@href]').extract()
+
+            person_name = re.match('(.*)\>(.*)\<(.*)', employee_details[7]).group(2)
+            phone = re.match('(.*)\>(.*)\<(.*)', employee_details[2]).group(2)
+            title = re.match('(.*)\>(.*)\<(.*)', employee_details[8]).group(2)
+            if re.search('@', email_extract[0]) != None:
+                email = re.match('(.*)mailto:(.*?)\"', email_extract[0]).group(2)
+            org_name = re.match('(.*)\>(.*)\<(.*)', org_name_extract[0]).group(2)
+
+            hierarchy = org_name
+            for layer in range(1, len(hierarchy_extract)-1):
+                print hierarchy_extract[layer]
+                layer_name = re.match('(.*)\>(.*)\<(.*)', hierarchy_extract[layer]).group(2)
+                hierarchy += " - " + layer_name
+
+
+            print person_name, phone, title, email, org_name
+            print hierarchy
+
+            print '\n'
 
 
 
+            output_line = ('"' + person_name + '"' + ',' +
+                            '"' + title + '"' + ',' +
+                            '"' + phone + '"' + ',' +
+                            '"' + email + '"' + ',' +
+                            '"' + org_name + '"' + ',' +
+                            '"' + hierarchy + '"')
+            file_dest.write(output_line + '\n')
 
-            print "Employee details"
-            print employee_details
-            # for item in employee_id:
-            #     #id_step0 = str(item)
-            #     #print id_step0
-            #     id_number = re.match("(.*)Employee\(\'(\d+)(.*)", str(item)).group(2)
-            #     print id_number
-            #     file_dest.write(id_number + '\n')
-            #file_dest.write('\n'.join(employee_id))
-            #file_dest.write('\n')
+
+        file_dest.close()
+
 
